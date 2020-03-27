1bdbd79c5be277a43e904427591f297ca0604f70
Rocky Eastman <eastmanrjr@gmail.com>
2020-03-27 12:34:04 -0700

    Install Laravel Vapor, hook up React

--- resources/js/bundles/Draft.tsx
+++ resources/js/bundles/Draft.tsx
@@ -6,11 +6,7 @@ import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux'
 import thunkMiddleware, { ThunkMiddleware } from 'redux-thunk'
-import { useMediaQuery } from 'react-responsive'
 import { appReducer, IAppState } from '@/state'
-
-import Desktop from '@desktop/Desktop'
-import Mobile from '@mobile/Mobile'
 
 //-----------------------------------------------------------------------------
 // Component
@@ -18,15 +14,12 @@ import Mobile from '@mobile/Mobile'
 export const Root = () => {
 
   const store = createStore(appReducer, applyMiddleware(thunkMiddleware as ThunkMiddleware<IAppState>))
-  
-  const isMobile = useMediaQuery({ query: '(max-width: 480px)' })
-  const forceDesktopApp = false
 
   return (
     <ReduxProvider store={store}>
-      {isMobile && !forceDesktopApp
-        ? <Mobile />
-        : <Desktop />}
+      <div>
+      Old School Fantasy baseball
+      </div>
     </ReduxProvider>
   )
 }
